533c88ae3808bfe618dbf23dd64aff9dbfd94da2
ArunadhithyaGowthaman <arunadhithya1907@gmail.com>
2026-03-30 12:25:57 +0530

    Specify base package for GlobalExceptionHandler

--- system/src/main/java/com/complaint/system/exception/GlobalExceptionHandler.java
+++ system/src/main/java/com/complaint/system/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.complaint.system")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
